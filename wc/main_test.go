f7f24fb6a4dce14c3e0f5748a218a104acbf5cf7
Berny Linville <bernylinville@devopsthink.org>
2022-01-20 16:30:34 +0800

    add command-line flags

--- wc/main_test.go
+++ wc/main_test.go
@@ -9,7 +9,17 @@ import (
 func TestCountWords(t *testing.T) {
 	b := bytes.NewBufferString("The quick brown fox jumps over the lazy dog!\n")
 	exp := 9
-	res := count(b)
+	res := count(b, false)
+	if res != exp {
+		t.Errorf("Expected %d, got %d instead.\n", exp, res)
+	}
+}
+
+// TestCountLines tests the count function set to count lines
+func TestCountLines(t *testing.T) {
+	b := bytes.NewBufferString("The quick brown\nfox jumps over the lazy\ndog!")
+	exp := 3
+	res := count(b, true)
 	if res != exp {
 		t.Errorf("Expected %d, got %d instead.\n", exp, res)
 	}
